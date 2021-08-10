9814e663167febc75bf3e5403242bb1c058be4a6
Aleksandar Ristic <storiaca@gmail.com>
2021-08-10 16:28:35 +0200

    Create kay value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+   let mut args = std::env::args().skip(1);
+   let key = args.next().unwrap();
+   let value = args.next().unwrap();
+   println!("Key: {} Value: {}", key, value);
 }
